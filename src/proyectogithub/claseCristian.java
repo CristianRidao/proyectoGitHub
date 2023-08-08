be4be428e20ebfa0963c524060134820ff76ecc5
CristianRidao <cristianridao@gmail.com>
2023-08-08 12:38:02 -0300

    claseCristian

---
+++ src/proyectogithub/claseCristian.java
@@ -0,0 +1,18 @@
+package proyectogithub;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author barzo
+ */
+public class claseCristian {
+     public static void metodo2(){
+        System.out.println("agregue una clase");
+        
+    }
+}
