5889d3d1b9cfd9bebc8a9d2f3a22080e2e56e3d4
Valentino1204 <valentinobulgarelli67@gmail.com>
2023-08-08 12:27:41 -0300

    Cambios valentino
    Prueba para ver texto de descripcion

---
+++ src/proyectogithub/ClaseValentino.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectogithub;
+
+/**
+ *
+ * @author valentinobulgarelli
+ */
+public class ClaseValentino {
+    
+    public static void metodo(){
+        System.out.println("Hola");
+    }
+}
